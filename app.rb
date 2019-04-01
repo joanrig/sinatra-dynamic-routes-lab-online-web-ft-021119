4ce092390c540f74efadb3aa81ee8e4c299144c8
github username <github email address>
2019-04-01 11:53:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -16,7 +16,19 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    erb :say_phrase
+    erb :say
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @params1 = params
+    erb :concat
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    erb :operation
   end
 
 end
