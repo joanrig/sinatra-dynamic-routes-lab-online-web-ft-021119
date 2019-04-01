01dc8f73d84539e6e3a4a185d4bce9218339bab1
github username <github email address>
2019-03-31 20:55:09 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,20 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    @name = params[:name]
+    erb :reversename
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    erb :square
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    erb :say_phrase
+  end
+
 end
